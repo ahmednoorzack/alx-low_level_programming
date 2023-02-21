5daa67820935b836be4226c462630ea1defe8200
ahmednoorzack <ahmednoorzakaria98@gmail.com>
2023-02-21 09:01:43 -0800

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  * main - Entry point
  *
@@ -6,6 +6,14 @@
  */
 int main(void)
 {
-	printf("_putchar \n");
+	char *sh = "Holberton";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
